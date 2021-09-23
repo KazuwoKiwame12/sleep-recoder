3c485b9c13b401ed42eef323c989c9503d080d08
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-09-23 17:41:22 +0900

    repair struct field name

--- bedin-time/database/database.go
+++ bedin-time/database/database.go
@@ -14,7 +14,7 @@ type SleepRecord struct {
 	Date     time.Time `json:"date"`
 	UserID   string    `json:"user_id"`
 	TimeB    int64     `json:"time_bedin"`
-	TimeA    int64     `json:"time_awake"`
+	TimeW    int64     `json:"time_wake"`
 	Duration float64   `json:"duration"`
 }
 
