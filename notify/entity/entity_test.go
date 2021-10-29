af353c02eadc9e2fd7b8bc0e033ab572b9e0f7c9
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-10-29 20:30:22 +0900

    test(notify): test for retrieve userIDs func

--- notify/entity/entity_test.go
+++ notify/entity/entity_test.go
@@ -14,3 +14,41 @@ func TestAdjustDuration(t *testing.T) {
 		t.Errorf("error: result = %v, want = %v", sr.Duration, want)
 	}
 }
+
+func Test_RetrieveUserIDs(t *testing.T) {
+	tests := []struct {
+		name  string
+		input entity.SleepRecords
+		want  []string
+	}{
+		{
+			name: "no duplicate",
+			input: entity.SleepRecords{
+				{UserID: "3"},
+				{UserID: "1"},
+				{UserID: "2"},
+			},
+			want: []string{"1", "2", "3"},
+		},
+		{
+			name: "duplicate",
+			input: entity.SleepRecords{
+				{UserID: "2"},
+				{UserID: "1"},
+				{UserID: "2"},
+			},
+			want: []string{"1", "2"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := test.input.RetrieveUserIDs()
+			for i := 0; i < len(result); i++ {
+				if result[i] != test.want[i] {
+					t.Errorf("unmatched error: result[%d] is %v, want[%d] is %v", i, result[i], i, test.want[i])
+				}
+			}
+		})
+	}
+}
