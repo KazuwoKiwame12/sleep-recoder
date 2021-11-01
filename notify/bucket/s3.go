309f337ef4638f568532e371c3df8e2bcf0e7a6d
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-11-01 17:02:51 +0900

    fix(notify): update

--- notify/bucket/s3.go
+++ notify/bucket/s3.go
@@ -36,7 +36,7 @@ func (i *ImageUploader) UploadImage(imagePath string) (string, error) {
 		Body:   image,
 	})
 
-	url, err := req.Presign(24 * 5 * time.Hour)
+	url, err := req.Presign(15 * time.Minute)
 	if err != nil {
 		return "", err
 	}
